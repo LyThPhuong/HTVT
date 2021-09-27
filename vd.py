57cb3e6bd5a42084a18c13a8b1ebaa73792aa4d1
LyThPhuong <lythuyphuong11@gmail.com>
2021-09-27 08:57:39 +0700

    da chinh lai tan so cat cua BPF- nguoi tiep theo lam giai dieu che

--- vd.py
+++ vd.py
@@ -37,6 +37,17 @@ def BPF(X,fcut):
     Xl[abs(f)>fcut ]=0
     Xlsau= X-Xl
     return(Xl,Xlsau)
+def LPF(X,fcut):
+    #X là tín hiệu cần lọc 
+    #f= fftfreq(n,1/fs)
+    #fcut = f của tín hiệu + khoảng bảo vệ
+    #lọc lần lượt tín hiệu có f tăng dần
+    #tín hiệu lọc tiếp theo
+    f = fftfreq(len(X), d=1./fs)
+    Xl = X.copy()
+    Xl[abs(f)<fcut ]=0
+    Xlsau= X-Xl
+    return(Xl,Xlsau)
 def dieucheAM(x,fc):
     A = float(input("Biên độ sóng mang: "))
     songmang = A*cos(2*pi*fc*t)
@@ -45,16 +56,28 @@ def dieucheAM(x,fc):
         xc[i]= songmang[i]*(1+u*x[i])
     return (xc)
 u = float(input("nguy ="))
+n=2*N
 x1,f1= ngovaocos()
 fsc1=2*f1
 x2,f2=ngovaosin()
 xc1= dieucheAM(x1,fsc1)
 fsc2 =2*f2 +fsc1+5
 xc2= dieucheAM(x2,fsc2)
+Xc1,Fc1=biendoiFourier(xc1,N)
+Xc2,Fc2=biendoiFourier(xc2,N)
 xc=xc1+xc2
-n=2*N
 Xc,Fc= biendoiFourier(xc,n)
-Xl1,XL1sau= BPF(Xc,fsc1)
+Xl1,Xl1sau= BPF(Xc,fsc1+f1+4)
+Xl2,Xl2sau= BPF(Xl1sau,fsc2+f2+4)
+#điều chế xong rồi mới lọc LPF nha 
+Xlc1,Xlc2 = LPF(Xc1,f1)
+subplot(311)
 plot(Fc,Xl1)
+plt.xlim(-200, 200)
+subplot(312)
+plot(Fc1,Xlc1)
+plt.xlim(-200, 200)
+subplot(313)
+plot(Fc,Xc)
 plt.xlim(-200, 200)
 show()
