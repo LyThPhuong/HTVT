48df0920dad5ddd8fe8bfef00effbb67fa8b80ae
LyThPhuong <lythuyphuong11@gmail.com>
2021-10-07 23:46:00 +0700

    Da chay duoc hoan chinh, mn chay lai thu nha

--- edit2.py
+++ edit2.py
@@ -1,124 +1,106 @@
+from scipy.fft import fft,fftfreq,ifft
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib as plt
+from numpy import sin,cos,pi,abs
 from matplotlib.pyplot import plot, subplot, grid,show
-from matplotlib.pyplot import ylabel, xlabel
-from numpy import sin,cos,pi,arctan
-from scipy.fft import fft,fftfreq
-
-f_laymau= 100 #KHz #số điểm tín hiệu sử dụng biểu diễn sóng
-duration = 10  #ms #Chiều dài của mẫu được tạo
-N= f_laymau*duration
-t=np.linspace(-duration,duration,f_laymau*duration)
-#ham sinc
-def ngovaosinc():
-    A = float(input("Biên độ: "))
-    f = float(input("Tần số: ")) #KHz
-    wt= 2*pi*f*t
-    x= A*np.sinc(wt)
-    return(x,f)
-def ngovaoxungvuong():
-    A = float(input("Biên độ: "))
-    f = float(input("Tần số: ")) 
-    T=2/f
-    Pi=[]
-    for i in t:
-        if i >= -T/2 and i <= T/2:
-            Pi.append(A)
-        else:
-            Pi.append(0)
-    return(Pi,f)
-def ngovaotamgiac():
-    A = float(input("Biên độ: "))
-    f = float(input("Tần số: ")) 
-    tg=[]
-    T=2/f
-    a =-2*A/T
-    b= A
-    for i in t:
-        if i >=0 and i <=T/2:
-            tg.append(a*i+b)
-        elif i>= -T/2 and i<=0:
-            tg.append(-a*i +b)
-        else:
-            tg.append(0)
-    return(tg,f)
+from matplotlib.pyplot import ylabel, xlabel,stem
+# sampling rate
+fs = 1000
+# sampling interval
+ts = 1.0/fs
+duration=5
+t = np.arange(0,duration,ts)
+tc=np.arange(0,2*duration,ts)
+N= fs*duration
+#ngõ vào
 def ngovaocos():
     A = float(input("Biên độ: "))
     f = float(input("Tần số: ")) #KHz
     x= A*cos(2*pi*f*t)
     return(x,f)
-
 def ngovaosin():
     A = float(input("Biên độ: "))
     f = float(input("Tần số: ")) #KHz
     x= A*sin(2*pi*f*t)
     return(x,f)
-def biendoiFourier(xc,n):
-    Xf= np.abs(fft(xc))/len(xc)
-    f= fftfreq(n,1/f_laymau) #Tính tần số ở tâm của mỗi khối fft
-    return(Xf,f)
+def biendoiFourier(x,n):
+    X= (fft(x))/len(x)
+    f= fftfreq(n,1/fs) #Tính tần số ở tâm của mỗi khối fft
+    return(X,f)
+def BPF(X,fc,f):
+    #X là tín hiệu cần lọc 
+    #f= fftfreq(n,1/fs)
+    #fcut = f của tín hiệu + khoảng bảo vệ
+    #lọc lần lượt tín hiệu có f tăng dần
+    #tín hiệu lọc tiếp theo
+    fx = fftfreq(len(X), d=1./fs)
+    Xl = X.copy()
+    Xl[abs(fx)< (fc-f)]=0 
+    Xl[abs(fx)>(fc+f)]=0
+    Xlsau= X-Xl
+    return(Xl,Xlsau)
+def LPF(X,fcut):
+    #X là tín hiệu cần lọc 
+    #f= fftfreq(n,1/fs)
+    #fcut = f của tín hiệu + khoảng bảo vệ
+    #lọc lần lượt tín hiệu có f tăng dần
+    #tín hiệu lọc tiếp theo
+    f = fftfreq(len(X), d=1./fs)
+    Xl = X.copy()
+    Xl[abs(f)>fcut ]=0
+    Xlsau= X-Xl
+    return(Xl,Xlsau)
 def dieucheAM(x,fc):
     A = float(input("Biên độ sóng mang: "))
-    #fsc = int(input("Tần số sóng mang: ")) #KHz
-    t= np.linspace(0,duration,f_laymau*duration)
     songmang = A*cos(2*pi*fc*t)
     xc=[None]*len(songmang)
     for i in range(0,len(xc)):
         xc[i]= songmang[i]*(1+u*x[i])
-    return (xc)
-
-#dieuchexungvuong
+    return (xc,A)
+def biendoiFouriernguoc(Xf):
+    xc= ifft(Xf)
+    return(xc)
+def giaidieuche(Xf,fc,Ac):
+    songmang = Ac*cos(2*pi*fc*tc)
+    yd=  xc *songmang 
+    return(yd)
 u = float(input("nguy ="))
-x1,f1= ngovaocos()
-fsc= 2*f1
-xc1=dieucheAM(x1,fsc)
-#dieuchesongsin
+print("Hãy nhập các tín hiệu có tần số theo thứ tự tăng dần")
+n=2*N
+print("Tín hiệu x1(t)")
+x1,f1=ngovaocos()
+fc1=2*f1
+xc1,Ac1=dieucheAM(x1,fc1)
+Xc1,Fc1=biendoiFourier(xc1,N)
+print("Tín hiệu x2(t)")
 x2,f2=ngovaosin()
-fsc += 2*f2+5
-xc2 =dieucheAM(x2,fsc)
-'''#dieuchesongcos
-x3,f3=ngovaocos()
-fsc += 2*f3+5
-xc3 =dieucheAM(x3,fsc)
-#dieuchexungtamgiac
-x4,f4=ngovaosin()
-fsc += 2*f4+5
-xc4 =dieucheAM(x4,fsc)'''
-xc=xc1+xc2#+xc3+xc4
-Xc1,fc11= biendoiFourier(xc1,N)
-Xc2,fc12= biendoiFourier(xc2,N)
-n= 2*N
-Xc,fc1=biendoiFourier(xc,n)
-Xcc= Xc1+Xc2
-subplot(4,1,3)
-plot(fc11,Xc1)
-ylabel("Xc1(f)")
-subplot(4,1,2)
-plot(fc12,Xc2)
-ylabel("Xc2(f)")
-subplot(4,1,1)
-plot(fc1,Xc)
-ylabel("Xc(f)=biendoifourier của xc1+xc2")
-subplot(4,1,4)
-plot(fc11,Xcc)
-ylabel("Xc(f)=Xc1+Xc2")
-'''sinx,f2= ngovaosin()
-x=sinx
-fsc=fsc1 + f1 + 5
-xc2=dieucheAM(x)
-#dieuchesongcos
-cosx,f3= ngovaocos()
-x=cosx
-fsc=fsc + f2 + 5
-xc3=dieucheAM(x)
-#dieuchexungtamgiac
-x1,f4= ngovaotamgiac()
-x=x1
-fsc=fsc + f3 + 5
-xc4=dieucheAM(x)
-#tinhieuFDM
-xc = xc1 + xc2 + xc3
-print(len(xc))
-xf1,f1 = biendoiFourier(xc)
-plot(xf1)'''
+fc2=fc1+ 2*f2 +5
+xc2,Ac2=dieucheAM(x2,fc2)
+Xc2,Fc2=biendoiFourier(xc2,N)
+xc=xc1+xc2
+Xc,Fc=biendoiFourier(xc,n)
+subplot(421)
+plot(tc,xc)
+subplot(422)
+plot(Fc,abs(Xc))
+Xl1,Xlsau1=BPF(Xc,fc1,f1)
+subplot(423)
+plot(Fc,abs(Xl1))
+y1=biendoiFouriernguoc(Xl1)
+yd1=giaidieuche(y1,fc1,Ac1)
+Yd1,Fd1=biendoiFourier(yd1,n)
+Yl1,Ylsau1=LPF(Yd1,f1)
+Yl1,Ylsau1=BPF(Yl1,f1,0)
+subplot(424)
+plot(Fd1,abs(Yl1))
+Xl2,Xlsau2=BPF(Xlsau1,fc2,f2)
+subplot(425)
+plot(Fc,abs(Xl2))
+y2=biendoiFouriernguoc(Xl2)
+yd2=giaidieuche(y2,fc2,Ac2)
+Yd2,Fd2=biendoiFourier(yd2,n)
+Yl2,Ylsau2=LPF(Yd2,f2)
+Yl2,Ylsau2=BPF(Yl2,f2,0)
+subplot(426)
+plot(Fd2,abs(Yl2))
 show()
